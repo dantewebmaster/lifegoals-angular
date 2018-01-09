f12344b914041b61d2cf409b09bfa9ddb68ca23e
Dante Webmaster <dantewebmaster@outlook.com>
2018-01-09 05:55:16 -0200

    Sort btn added, responsive improvement, active class on menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,18 +13,19 @@ import { DataService } from '../data.service';
 			transition('* => *', [
 				query(':enter', style({ opacity: 0 }), { optional: true }),
 
-				query(':enter', stagger('300ms', [
+				query(':enter', stagger('200ms', [
 					animate('.6s ease-in', keyframes([
 						style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
-						style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+						style({ opacity: 0.5, transform: 'translateY(35px)', offset: .3 }),
 						style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
 					]))
 				]), { optional: true }),
-				query(':leave', stagger('300ms', [
+
+				query(':leave', stagger('200ms', [
 					animate('.6s ease-in', keyframes([
 						style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
-						style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
-						style({ opacity: 0, transform: 'translateY(75%)', offset: 1 })
+						style({ opacity: 0.5, transform: 'translateY(35px)', offset: .3 }),
+						style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 })
 					]))
 				]), { optional: true })
 			])
@@ -56,6 +57,10 @@ export class HomeComponent implements OnInit {
 	removeItem(i) {
 		this.goals.splice(i, 1);
 		this._data.changeGoal(this.goals);
+	}
+
+	sortList() {
+		this.goals.reverse();
 	}
 
 }
